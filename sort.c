152f2eea642ef0afd3b192f525fed18088a123bc
Colin S. <crszbt@umsystem.edu>
2025-09-29 21:47:24 -0500

    Update sort.c
    Copied changes from Hellbender to Github

--- sort.c
+++ sort.c
@@ -1,8 +1,20 @@
 #include <cs3050.h>
-void heapsort(void *array, 
-				size_t nitems, 
-				size_t size, 
-				int (*CompareFunc)(const void *, const void*))
+void compareandswap(void *array, int i, size_t size)
 {
-	//testing testing 123
+	if(CompareFunc(array[i],array[i/2])>0 && i!=1)
+		{
+			Swap(array[i], array[i/2], size);
+			compareandswap(*array, i/2, size);
+		}
+}
+
+void heapsort(void *array, //array to be sorted
+				size_t nitems, //number of array items
+				size_t size, //size of each array item
+				int (*CompareFunc)(const void *, const void*)) //compare function
+{
+	for(int i=1,i=<nitems,i++)
+	{
+		compareandswap(*array, i, size);
+	}
 }
