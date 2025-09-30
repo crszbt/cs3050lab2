17f52358b0bbbee2fe66cd7bbf0b219f0e9d9806
Colin S. <crszbt@umsystem.edu>
2025-09-30 11:07:28 -0500

    Added comments
    Because I forgot to last time

--- sort.c
+++ sort.c
@@ -1,10 +1,10 @@
 #include <cs3050.h>
 void compareandswap(void *array, int i, size_t size)
 {
-	if(CompareFunc(array[i],array[i/2])>0 && i!=1)
+	if(CompareFunc(array[i],array[i/2])>0 && i!=1) //if the item being looked at isn't the root and isn't in the right place...
 		{
-			Swap(array[i], array[i/2], size);
-			compareandswap(*array, i/2, size);
+			Swap(array[i], array[i/2], size); //swap with the parent node
+			compareandswap(*array, i/2, size); //check new location
 		}
 }
 
@@ -13,8 +13,8 @@ void heapsort(void *array, //array to be sorted
 				size_t size, //size of each array item
 				int (*CompareFunc)(const void *, const void*)) //compare function
 {
-	for(int i=1,i=<nitems,i++)
+	for(int i=1,i=<nitems,i++) //for each item in the list...
 	{
-		compareandswap(*array, i, size);
+		compareandswap(*array, i, size); //compare and swap if necessary
 	}
 }
