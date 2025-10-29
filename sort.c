ae608e427e1af82586e29da5b233494926e8485c
Colin S. <crszbt@umsystem.edu>
2025-10-28 21:35:35 -0500

    Update sort.c to actually work

--- sort.c
+++ sort.c
@@ -1,10 +1,10 @@
 #include <cs3050.h>
-void compareandswap(void *array, int i, size_t size)
+void compareandswap(void *array, int i, size_t size, int (*CompareFunc)(const void *, const void*))
 {
-	if(CompareFunc(array[i],array[i/2])>0 && i!=1) //if the item being looked at isn't the root and isn't in the right place...
+	if(i!=0 && CompareFunc(array+i*size,array+((i-1)/2)*size)<0) //if the item being looked at isn't the root and isn't in the right place...
 		{
-			Swap(array[i], array[i/2], size); //swap with the parent node
-			compareandswap(*array, i/2, size); //check new location
+			Swap(array+i*size,array+((i-1)/2)*size, size); //swap with the parent node. 
+			compareandswap(array, (i-1)/2, size, CompareFunc); //check new location
 		}
 }
 
@@ -13,8 +13,13 @@ void heapsort(void *array, //array to be sorted
 				size_t size, //size of each array item
 				int (*CompareFunc)(const void *, const void*)) //compare function
 {
-	for(int i=1,i=<nitems,i++) //for each item in the list...
+	while(nitems>0)
 	{
-		compareandswap(*array, i, size); //compare and swap if necessary
+		for(int i=0;i<nitems;i++) //for each item in the list...
+		{
+		compareandswap(array, i, size, CompareFunc); //compare and swap if necessary
+		}
+		Swap(array,array+(nitems-1)*size, size); //places front at back of list
+		nitems--; //decreases the number of unsorted nodes by 1
 	}
 }
